43cdba496f72d115fd7b4303155decd16d778f63
123SourLemon123 <sd.russia@gmail.com>
2024-11-28 02:31:44 +0300

    Add files via upload

--- app/lib/upstash.ts
+++ app/lib/upstash.ts
@@ -1,15 +1,14 @@
 import { Redis } from '@upstash/redis'
 
-if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
-  throw new Error('KV_REST_API_URL and KV_REST_API_TOKEN must be set')
-}
-
 let redis: Redis | null = null;
 
 export async function getRedisClient() {
+  if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
+    throw new Error('Необходимо настроить переменные окружения KV_REST_API_URL и KV_REST_API_TOKEN в настройках проекта на Vercel');
+  }
+
   if (!redis) {
     console.log('Creating new Redis client...');
-    console.log('Using KV_REST_API_URL:', process.env.KV_REST_API_URL);
     redis = new Redis({
       url: process.env.KV_REST_API_URL,
       token: process.env.KV_REST_API_TOKEN,
