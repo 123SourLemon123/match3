028b74f66b60195daebe969627be57969149e2cd
123SourLemon123 <sd.russia@gmail.com>
2024-11-28 03:12:51 +0300

    Update debug.ts

--- app/actions/debug.ts
+++ app/actions/debug.ts
@@ -1,9 +1,1 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    KV_REST_API_URL: process.env.KV_REST_API_URL ? 'Set' : 'Not set',
-    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN ? 'Set' : 'Not set',
-  })
-}
 
